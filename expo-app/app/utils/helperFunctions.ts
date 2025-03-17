4bee5061d300fd35cd1c5706b5491a116f722a63
Gderhy <gabrielderhy@gmail.com>
2025-03-17 14:28:11 -0400

    fix(CampusMap): refactor getFillColorWithOpacity function to improve opacity handling and move hexToRgba utility

--- expo-app/app/utils/helperFunctions.ts
+++ expo-app/app/utils/helperFunctions.ts
@@ -7,7 +7,9 @@ export const getFillColorWithOpacity = (
 ) => {
   // If the user is inside the building, make it red
   if (currentBuilding && currentBuilding.id === building.id) {
-    return concordiaBurgendyColor;
+    const opacity = selectedBuilding?.id === building.id ? 1 : 0.8;
+    const concordiaBurgendyColorRGBA = hexToRgba(concordiaBurgendyColor, opacity);
+    return concordiaBurgendyColorRGBA.replace(/[\d\.]+\)$/, `${opacity})`);
   }
 
   // Get the building's original fill color
@@ -15,17 +17,16 @@ export const getFillColorWithOpacity = (
 
   // Convert hex color to rgba if necessary
   if (fillColor.startsWith("#")) {
-    const hexToRgba = (hex: string, alpha: number) => {
-      const r = parseInt(hex.slice(1, 3), 16);
-      const g = parseInt(hex.slice(3, 5), 16);
-      const b = parseInt(hex.slice(5, 7), 16);
-      return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-    };
-
     fillColor = hexToRgba(fillColor, 1); // Default to full opacity
   }
-
   // Apply opacity filter if the building is not selected
   const opacity = selectedBuilding?.id === building.id ? 1 : 0.4;
   return fillColor.replace(/[\d\.]+\)$/, `${opacity})`);
+};
+
+const hexToRgba = (hex: string, alpha: number) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
 };
