174691d4deeb11671195a91456dde59992cdf91f
Gderhy <gabrielderhy@gmail.com>
2025-03-17 19:13:20 -0400

    Refactor IndoorMap component for improved code readability

--- expo-app/app/screens/IndoorMap/IndoorMap.tsx
+++ expo-app/app/screens/IndoorMap/IndoorMap.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import { MiMapView } from '@mappedin/react-native-sdk';
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { MiMapView } from "@mappedin/react-native-sdk";
 
 // See Trial API key Terms and Conditions
 // https://developer.mappedin.com/docs/demo-keys-and-maps
 
 const options = {
-    //Details for demo map (just for testing)
-	clientId: '5eab30aa91b055001a68e996',
-    clientSecret: 'RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1',
-    venue: 'mappedin-demo-mall',
-	perspective: 'Website',
-	
-	//Details for GoldenDoodle map
-	//mapId: '67c87db88e15de000bed1abb',
-	//key: 'mik_LUeYSdqlvDJYpSHXT56b207a8',
-	//secret: 'mis_toNhLLj9mmlK5rJzJHebi3km4rjI9V3x3VUzgpPCYbq9a2410c7',
+  //Details for demo map (just for testing)
+  clientId: "5eab30aa91b055001a68e996",
+  clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1",
+  venue: "mappedin-demo-mall",
+  perspective: "Website",
+
+  //Details for GoldenDoodle map
+  //mapId: '67c87db88e15de000bed1abb',
+  //key: 'mik_LUeYSdqlvDJYpSHXT56b207a8',
+  //secret: 'mis_toNhLLj9mmlK5rJzJHebi3km4rjI9V3x3VUzgpPCYbq9a2410c7',
 };
 
 const App = () => {
-    return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<Text>Test</Text>
-            <MiMapView style={{ flex: 1 }} key="mappedin" options={options} />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Test</Text>
+      <MiMapView style={{ flex: 1 }} key="mappedin" options={options} />
+    </SafeAreaView>
+  );
 };
 
 export default App;
