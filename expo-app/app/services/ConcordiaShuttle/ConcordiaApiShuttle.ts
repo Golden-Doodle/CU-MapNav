af1f83b2e5569c8b09a6d6487cb4b972ed602527
Gderhy <gabrielderhy@gmail.com>
2025-04-04 16:09:23 -0400

    Add fetchBusCoordinates function to retrieve bus coordinates from API

--- expo-app/app/services/ConcordiaShuttle/ConcordiaApiShuttle.ts
+++ expo-app/app/services/ConcordiaShuttle/ConcordiaApiShuttle.ts
@@ -1,3 +1,5 @@
+import { BusObject, ShuttlePointObject } from "@/app/utils/types";
+
 export const fetchBusLocations = async () => {
   const url = `https://us-central1-soen-390-golden-doodle.cloudfunctions.net/api/v1/bus-locations`;
 
@@ -12,10 +14,38 @@ export const fetchBusLocations = async () => {
     }
 
     const data = await response.json();
-    console.log("Bus Locations Data:", data.d); // Debugging log
     return data;
   } catch (error) {
     console.error("Error fetching bus locations:", error);
+    throw error;
+  }
+};
+
+export const fetchBusCoordinates = async () => {
+  try {
+    const data = await fetchBusLocations();
+
+    const Points: ShuttlePointObject[] = data.d.Points;
+
+    console.log("Bus Coordinates Data:", Points); // Debugging log
+
+    const busObjects: BusObject[] = Points.map((point: ShuttlePointObject) => {
+      if (point.ID.includes("BUS")) {
+        return {
+          id: point.ID,
+          coordinates: {
+            latitude: point.Latitude,
+            longitude: point.Longitude,
+          },
+          status: point.PointStatus,
+        };
+      }
+      return null;
+    }).filter((bus) => bus !== null);
+
+    return busObjects;
+  } catch (error) {
+    console.error("Error fetching bus coordinates:", error);
     throw error;
   }
 };
