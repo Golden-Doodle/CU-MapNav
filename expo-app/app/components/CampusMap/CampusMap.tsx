73707b10cecac12fe2b4a5a0a0e891aefe248601
Gderhy <gabrielderhy@gmail.com>
2025-04-04 16:19:49 -0400

    Add live shuttle location feature to CampusMap component

--- expo-app/app/components/CampusMap/CampusMap.tsx
+++ expo-app/app/components/CampusMap/CampusMap.tsx
@@ -29,6 +29,7 @@ import {
   CustomMarkerType,
   Building,
   GooglePlace,
+  BusObject,
 } from "@/app/utils/types";
 import { useTranslation } from "react-i18next";
 import RadiusAdjuster from "./RadiusAdjuster";
@@ -36,6 +37,7 @@ import { getCustomMapStyle } from "./styles/MapStyles";
 import { calculateDistance, isPointInPolygon } from "@/app/utils/MapUtils";
 import { getFillColorWithOpacity } from "@/app/utils/helperFunctions";
 import IndoorMap from "@/app/components/IndoorNavigation/IndoorMap";
+import { fetchBusCoordinates } from "@/app/services/ConcordiaShuttle/ConcordiaApiShuttle";
 
 interface CampusMapProps {
   pressedOptimizeRoute: boolean;
@@ -68,6 +70,9 @@ const CampusMap = ({ pressedOptimizeRoute = false }: CampusMapProps) => {
 
   const markers = campus === "SGW" ? SGWMarkers : LoyolaMarkers;
   const buildings = campus === "SGW" ? SGWBuildings : LoyolaBuildings;
+
+  const [liveShuttleLocations, setLiveShuttleLocations] = useState<BusObject[]>([]);
+  const [displayLiveShuttleLocation, setDisplayLiveShuttleLocation] = useState<boolean>(false); 
 
   const { t } = useTranslation("CampusMap");
 
@@ -175,6 +180,30 @@ const CampusMap = ({ pressedOptimizeRoute = false }: CampusMapProps) => {
       setIsNextClassModalVisible(true);
     }
   }, [pressedOptimizeRoute]);
+
+  useEffect(() => {
+    if(!displayLiveShuttleLocation) {
+      return;
+    }
+
+    const fetchShuttleLocations = async () => {
+      if (!displayLiveShuttleLocation) {
+        return;
+      }
+      try {
+        const locations = await fetchBusCoordinates();
+        setLiveShuttleLocations(locations);
+        console.log("Fetched Shutasasatle Locations: ", liveShuttleLocations);
+      } catch (error) {
+        console.error("Error fetching shuttle locations: ", error);
+      }
+    };
+
+    fetchShuttleLocations();
+    const interval = setInterval(fetchShuttleLocations, 60000); // Fetch every 60 seconds
+    return () => clearInterval(interval);
+  }, [displayLiveShuttleLocation]);
+
 
   const handleMarkerPress = useCallback((marker: CustomMarkerType) => {
     const markerToBuilding: Building = {
@@ -315,6 +344,8 @@ const CampusMap = ({ pressedOptimizeRoute = false }: CampusMapProps) => {
         campus={campus}
         darkMode={isDarkMode}
         onDarkModeChange={setIsDarkMode}
+        displayLiveShuttleLocation={displayLiveShuttleLocation}
+        setDisplayLiveShuttleLocation={setDisplayLiveShuttleLocation}
       />
 
       <MapView
@@ -395,6 +426,18 @@ const CampusMap = ({ pressedOptimizeRoute = false }: CampusMapProps) => {
             markerType="default"
           />
         )}
+
+        {/* Display live shuttle location */}
+        {displayLiveShuttleLocation && liveShuttleLocations.map((location, index) => (
+          <CustomMarker
+            testID={`shuttle-marker-${index}`}
+            key={`shuttle-${index}`}
+            coordinate={location.coordinates}
+            title="Live Shuttle Location"
+            description="Live Shuttle Location"
+            markerType="shuttle"
+          />
+        ))}
       </MapView>
 
       {viewEatingOnCampus && (
